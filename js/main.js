b3cf4ccce5d6a8862a099335bd75118071809893
thekiwidev <adedotungabriel20@gmail.com>
2022-09-27 12:43:07 +0100

    deleted the non-needed splidejs files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+menu.addEventListener("click", () => {
+    menu.classList.toggle("open");
+    nav.classList.toggle("open");
+});
---
+++ main.js
@@ -0,0 +1,31 @@
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+
+const header = document.getElementById("header");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("open");
+  nav.classList.toggle("open");
+});
+
+// splide slider for the Students, Agents, & Schools
+
+new Splide(".splide", {
+  type: "loop",
+  // padding: "5%",
+  gap: "3%",
+  pauseOnHover: (boolean = true),
+  flickPower: 600,
+}).mount();
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    document.querySelector("ul.navlinks").classList.add("active");
+    document.querySelector("a.logo").classList.add("active");
+  } else {
+    header.classList.remove("active");
+    document.querySelector("ul.navlinks").classList.remove("active");
+    document.querySelector("a.logo").classList.remove("active");
+  }
+});
