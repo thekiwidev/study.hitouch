2e6406d0be7891ad73a5a3668ff7196ea34520ff
thekiwidev <adedotungabriel20@gmail.com>
2022-10-17 19:37:19 +0100

    deployed sanity studio, and to start working on schemas

---
+++ studyhitouch/src/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+    {
+      name: "netlify",
+      options: {
+        title: "My Netlify deploys",
+        sites: [
+          {
+            title: "HiTouch Global | Study Abroad",
+            apiId: "c4769259-b379-40af-8b8f-78fd84ac7077",
+            buildHookId: "xxxyyyxxxyyyyxxxyyy",
+            name: "study-hitouch",
+          },
+        ],
+      },
+    },
+  ],
+};
